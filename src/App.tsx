1ba655838bb4d4a11c025230e1e44dfaddad1b2f
Andrei Mocanu <andreimcornel@gmail.com>
2025-01-06 14:06:20 +0200

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-	return <>Init</>
+	return (
+		<div className='min-h-screen bg-gray-100 flex items-center justify-center'>
+			<h1 className='text-4xl font-bold text-blue-600'>Tiny browsers</h1>
+		</div>
+	)
 }
 
 export default App
